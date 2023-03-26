64334f9081579ff8b4780ec0e686116fa48eeb8f
Jordan Kay <jordantkay21@gmail.com>
2023-03-26 01:13:04 -0400

    Added Calculator UI

--- Assets/Calculator/Scripts/DisplayController.cs
+++ Assets/Calculator/Scripts/DisplayController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Input : MonoBehaviour
+public class DisplayController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
