c8904a2a966fa4ec7b88413ccb1936353d3cd007
Jordan Kay <jordantkay21@gmail.com>
2023-03-26 01:56:16 -0400

    Added Button/Display Controller Scripts

--- Assets/Calculator/Scripts/ButtonController.cs
+++ Assets/Calculator/Scripts/ButtonController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string value;
+    
+    /// <summary>
+    /// add the value of this button to the displayText
+    /// </summary>
+    public void AddValueToDisplay()
     {
-        
+        DisplayController.Instance.UpdateDisplayText(value);
     }
-
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Display answer method
+    /// </summary>
+    public void EvaluateEquation()
     {
-        
+        DisplayController.Instance.DisplayAnswer();
+    }
+    /// <summary>
+    /// Clear the DisplayText
+    /// </summary>
+    public void Clear()
+    {
+        DisplayController.Instance.ClearDisplay();
     }
 }
--- Assets/Calculator/Scripts/DisplayController.cs
+++ Assets/Calculator/Scripts/DisplayController.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class DisplayController : MonoBehaviour
+public class DisplayController : MonoSingleton<DisplayController>
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Init()
     {
-        
+        base.Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    private Text displayText;
+
+    private void Start()
     {
-        
+        displayText = GameObject.Find("DisplayText").GetComponent<Text>();
     }
+
+    /// <summary>
+    /// Sets the DisplayText to an empty string
+    /// </summary>
+    public void ClearDisplay()
+    {
+        displayText.text = "";
+    }
+
+    /// <summary>
+    /// Adds text to the end of the display
+    /// </summary>
+    /// <param name="newText">Button Value</param>
+    public void UpdateDisplayText(string newText)
+    {
+        displayText.text += newText;
+    }
+    /// <summary>
+    /// displays the answer to the equation
+    /// </summary>
+    public void DisplayAnswer()
+    {
+
+    }
+
+
 }
